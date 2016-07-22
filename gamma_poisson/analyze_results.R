fbef9292824f8f913d521b1ac198e1ae0d9c52c8
Ryan Giordano <rgiordan@gmail.com>
2016-07-22 11:31:26 -0400

    Comparison

--- gamma_poisson/analyze_results.R
+++ gamma_poisson/analyze_results.R
@@ -2,8 +2,6 @@ library(ggplot2)
 library(dplyr)
 library(reshape2)
 library(rstan)
-library(Matrix)
-library(boot) # For inverse logit
 
 project_directory <- file.path(Sys.getenv("GIT_REPO_LOC"), "EmpiricalBayes/gamma_poisson")
 data_directory <- file.path(project_directory, "data/")
@@ -12,3 +10,47 @@ stan_directory <- file.path(project_directory, "stan/")
 stan_draws_file <- file.path(data_directory, "gamma_poisson_mcmc_draws.Rdata")
 stan_results <- environment()
 load(stan_draws_file, env=stan_results)
+
+
+lambda_fixed <- extract(stan_results$fixed_stan_sim)
+lambda_free <- extract(stan_results$free_stan_sim)
+
+true_lambda <- stan_results$true_params$lambda
+lambda_true_df <- data.frame(g=1:length(true_lambda), "true_lambda"=true_lambda)
+
+lambda_free_df <-
+  melt(lambda_free$lambda) %>% rename(g=Var2, draw=iterations, lambda=value) %>%
+  mutate(method="free")
+lambda_fixed_df <-
+  melt(lambda_fixed$lambda) %>% rename(g=Var2, draw=iterations, lambda=value) %>%
+  mutate(method="fixed")
+lambda_df <- rbind(lambda_free_df, lambda_fixed_df)
+
+lambda_summary <-
+  group_by(lambda_df, method, g) %>%
+  summarize(mean=mean(lambda), sd=sd(lambda)) %>%
+  dcast(g ~ method, value.var="mean") %>%
+  inner_join(lambda_true_df, by="g")
+
+ggplot(lambda_summary) +
+    geom_point(aes(x=fixed, y=true_lambda, color="true")) +
+    geom_point(aes(x=fixed, y=free, color="free")) +
+    geom_abline(aes(intercept=0, slope=1))
+
+
+lambda_grouped <-
+  dcast(lambda_df, g + draw ~ method, value.var="lambda") %>%
+  melt(id.vars=c("g", "draw")) %>%
+  rename(method=variable, lambda=value) %>%
+  inner_join(lambda_true_df, by="g")
+
+rmse <-
+  group_by(lambda_grouped, g, method) %>%
+  summarize(abs_bias=abs(mean(lambda - true_lambda)),
+            sd=sd(lambda - true_lambda), 
+            rmse=sqrt(mean((lambda - true_lambda) ^ 2))) %>%
+  melt(id.vars=c("g", "method")) %>%
+  rename(measure=variable)
+
+group_by(rmse, method, measure) %>% summarize(mean=mean(value))
+dcast(rmse, g ~ measure + method)
--- gamma_poisson/stan/gamma_poisson_free_prior.stan
+++ gamma_poisson/stan/gamma_poisson_free_prior.stan
@@ -1,17 +1,31 @@
 data {
   int<lower=0> N;  // total number of observations
   int<lower=0> y[N];       // Poisson draws
+  real prior_gamma_mean;
+  real prior_beta_mean;
+  real<lower=0> prior_gamma_var;
+  real<lower=0> prior_beta_var;
 }
 parameters {
+  real<lower=0> lambda[N];
   real<lower=0> prior_gamma;
   real<lower=0> prior_beta;
-  real<lower=0> lambda[N];
 }
 transformed parameters {
+  real<lower=0> gamma_alpha;
+  real<lower=0> gamma_beta;
+  real<lower=0> beta_alpha;
+  real<lower=0> beta_beta;
+
+  gamma_alpha <- (prior_gamma_mean ^ 2) / prior_gamma_var;
+  gamma_beta <- prior_gamma_mean / prior_gamma_var;
+  
+  beta_alpha <- (prior_beta_mean ^ 2) / prior_beta_var;
+  beta_beta <-prior_gamma_mean / prior_beta_var;
 }
 model {
-  prior_gamma ~ lognormal(0, 50);
-  prior_beta ~ lognormal(0, 50);
+  prior_gamma ~ gamma(gamma_alpha, gamma_beta);
+  prior_beta ~ gamma(beta_alpha, beta_beta);
   for (n in 1:N) {
     lambda[n] ~ gamma(prior_gamma, prior_beta);
     y[n] ~ poisson(lambda[n]);
